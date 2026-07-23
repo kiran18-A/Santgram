46e55dbdb23534c0a924b49262ec53ff5102bedd
kiran-A <kiranaglawe18@gmail.com>
2026-07-23 13:00:49 +0530

    change password

--- index.js
+++ index.js
@@ -363,7 +363,7 @@ const adminAuth = (req, res, next) => {
   const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
   const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
 
-  const adminPassword = process.env.ADMIN_PASSWORD || 'SantgramAdmin123';
+  const adminPassword = process.env.ADMIN_PASSWORD || '12345678';
   if (login && password && login === 'admin' && password === adminPassword) {
     return next();
   }
